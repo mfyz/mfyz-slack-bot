1d4dfdd1b3d452df826292fb2fe868dc8fcf9103
Mehmet Fatih Yıldız <fatih@mfyz.com>
2019-11-11 10:24:32 -0500

    slack event api - initial connection attempt

--- index.js
+++ index.js
@@ -8,4 +8,13 @@ app.get('/', function (req, res) {
 	res.json({ title: 'Hey', message: 'Hello there!' })
 })
 
+app.get('/slack-events', function (req, res) {
+	let payload = req.body;
+
+	// payload.type
+	// payload.token
+ 
+	res.send(payload.challenge);
+})
+
 app.listen(PORT, () => console.log(`App listening on port ${PORT}!`))
